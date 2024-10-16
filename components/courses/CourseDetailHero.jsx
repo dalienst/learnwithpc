82d4f05db4c43a08932d5e539c48c0bbcbf1fcf7
dalienst <oduordalienst@gmail.com>
2024-10-16 18:45:16 +0300

    modified hero section

--- components/courses/CourseDetailHero.jsx
+++ components/courses/CourseDetailHero.jsx
@@ -5,11 +5,11 @@ function CourseDetailHero({ course }) {
     return (
         <div className="m-auto flex justify-center bg-navy py-24">
             <div className="container mx-auto px-4 max-w-screen-xl">
-                <h1 className='font-bold font-playfair text-white font-display text-4xl mb-3'>{course?.title}</h1>
-                <div className="flex justify-start gap-2 flex-wrap">
-                    <p className='text-navy text-xl rounded-full bg-white px-4 py-1'>{course?.duration}</p>
-                    <p className='text-navy text-xl rounded-full bg-white px-4 py-1'>{course?.level}</p>
-                    <p className='text-navy text-xl rounded-full bg-white px-4 py-1'>{course?.method}</p>
+                <h1 className='font-bold font-playfair text-white font-display text-4xl mb-3 text-center'>{course?.title}</h1>
+                <div className="flex justify-center gap-2 flex-wrap">
+                    <p className='text-navy text-xl rounded-full bg-white px-4 hover:bg-navy hover:border hover:border-white hover:text-white py-1'>{course?.duration}</p>
+                    <p className='text-navy text-xl rounded-full bg-white px-4 hover:bg-navy hover:border hover:border-white hover:text-white py-1'>{course?.level}</p>
+                    <p className='text-navy text-xl rounded-full bg-white px-4 hover:bg-navy hover:border hover:border-white hover:text-white py-1'>{course?.method}</p>
                 </div>
             </div>
         </div>
