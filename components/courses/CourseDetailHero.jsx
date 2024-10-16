10d1ad82a22c719c4e026d636e058348ffe16acb
dalienst <oduordalienst@gmail.com>
2024-10-16 18:39:43 +0300

    display course detail

--- components/courses/CourseDetailHero.jsx
+++ components/courses/CourseDetailHero.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 function CourseDetailHero({ course }) {
     return (
-        <div className="m-auto flex justify-center bg-navy py-12">
-            <div className="container">
-                <h1 className='font-bold font-playfair text-white font-display text-4xl mb-3 text-center'>{course?.title}</h1>
-                <div className="flex justify-center gap-2 flex-wrap">
-                    <p className='text-navy text-xs rounded-full bg-white px-2 py-1'>{course?.duration}</p>
-                    <p className='text-navy text-xs rounded-full bg-white px-2 py-1'>{course?.level}</p>
-                    <p className='text-navy text-xs rounded-full bg-white px-2 py-1'>{course?.method}</p>
+        <div className="m-auto flex justify-center bg-navy py-24">
+            <div className="container mx-auto px-4 max-w-screen-xl">
+                <h1 className='font-bold font-playfair text-white font-display text-4xl mb-3'>{course?.title}</h1>
+                <div className="flex justify-start gap-2 flex-wrap">
+                    <p className='text-navy text-xl rounded-full bg-white px-4 py-1'>{course?.duration}</p>
+                    <p className='text-navy text-xl rounded-full bg-white px-4 py-1'>{course?.level}</p>
+                    <p className='text-navy text-xl rounded-full bg-white px-4 py-1'>{course?.method}</p>
                 </div>
             </div>
         </div>
