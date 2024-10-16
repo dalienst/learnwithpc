a581795d4bf71341923355bd0ae16fc6bdf552b3
dalienst <oduordalienst@gmail.com>
2024-10-16 18:51:54 +0300

    display course details

--- app/courses/[id]/page.jsx
+++ app/courses/[id]/page.jsx
@@ -10,23 +10,29 @@ function CourseDetail({ params: { id } }) {
         <>
             <CourseDetailHero course={course} />
             <main className="container mx-auto mb-3 px-4 py-3 max-w-screen-xl">
-                <section>
-                    <div className='mb-4'>
-                        <h5 className='text-3xl font-semibold'>Content in this course</h5>
-                        <ul className="list-disc list-inside px-4">
-                            {course?.content?.map((item) => (
-                                <li key={item} className='mb-1'>{item}</li>
-                            ))}
-                        </ul>
+                <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div>
+                        <div className='mb-4'>
+                            <h5 className='text-3xl font-semibold'>Content in this course</h5>
+                            <ul className="list-disc list-inside px-4">
+                                {course?.content?.map((item) => (
+                                    <li key={item} className='mb-1'>{item}</li>
+                                ))}
+                            </ul>
+                        </div>
+
+                        <div className='mb-4'>
+                            <h5 className='text-3xl font-semibold'>What you'll learn</h5>
+                            <ol className="list-decimal list-inside px-4">
+                                {course?.what_you_will_learn?.map((item) => (
+                                    <li key={item} className='mb-1'>{item}</li>
+                                ))}
+                            </ol>
+                        </div>
                     </div>
 
-                    <div className='mb-4'>
-                        <h5 className='text-3xl font-semibold'>What you'll learn</h5>
-                        <ol className="list-decimal list-inside px-4">
-                            {course?.what_you_will_learn?.map((item) => (
-                                <li key={item} className='mb-1'>{item}</li>
-                            ))}
-                        </ol>
+                    <div>
+                        <div className='mb-4'>hello</div>
                     </div>
                 </section>
             </main>
