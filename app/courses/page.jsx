014edec8e48fcb363a363ae4d872803c45c67ad2
dalienst <oduordalienst@gmail.com>
2024-10-16 15:09:38 +0300

    display all courses

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -1,10 +1,16 @@
+"use client";
 import Hero from '@/components/courses/Hero'
+import { allcourses } from '@/data/allcourses'
+import CourseCard from '@/layouts/CourseCard'
 import React from 'react'
 
 function Courses() {
   return (
     <>
-    <Hero />
+      <Hero />
+      <main className="container mx-auto px-4 py-3 max-w-screen-xl">
+        <CourseCard courses={allcourses} />
+      </main>
     </>
   )
 }
--- components/Courses.jsx
+++ components/Courses.jsx
@@ -8,11 +8,7 @@ function FeaturedCourses() {
     return (
         <div className="container mx-auto mb-3 px-4 py-3 max-w-screen-xl">
             <h2 className="font-lato text-center text-3xl font-bold">A-Level Courses</h2>
-            <div className="flex flex-wrap justify-center">
-                {alevel.map((course) => (
-                    <CourseCard course={course} />
-                ))}
-            </div>
+            <CourseCard courses={alevel} />
             <div className="flex justify-center mt-6">
                 <Link href="/courses" className="text-center bg-foreground text-white rounded px-4 py-2 hover:bg-white hover:text-foreground hover:border hover:border-black font-bold">
                     All Courses
